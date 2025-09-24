b53c65a1acb5518121a0664f9544c9b4ea64dfbd
SangavMut <smsangavi@gmail.com>
2025-09-24 18:46:52 +0530

    change deetction

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectorRef,
+  ChangeDetectionStrategy
+} from '@angular/core';
 import { Task, TaskService } from './task';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -9,7 +14,8 @@ import { HttpClientModule } from '@angular/common/http';
   standalone: true,
   imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './app.html',
-  styleUrls: ['./app.scss']
+  styleUrls: ['./app.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App implements OnInit {
   tasks: Task[] = [];
@@ -18,7 +24,10 @@ export class App implements OnInit {
   updatedTask: string = '';
   showNewTaskModal = false;
 
-  constructor(private taskService: TaskService) {}
+  constructor(
+    private taskService: TaskService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.loadTasks();
@@ -27,22 +36,24 @@ export class App implements OnInit {
   loadTasks(): void {
     this.taskService.getTasks().subscribe(data => {
       this.tasks = data;
+      this.cdr.markForCheck();
     });
   }
 
   addTask(): void {
     if (!this.newTask.trim()) return;
-    this.taskService.addTask({ task: this.newTask , id: null}).subscribe(() => {
+    this.taskService.addTask({ task: this.newTask, id: null }).subscribe(() => {
       this.newTask = '';
-      this.loadTasks();
       this.showNewTaskModal = false;
+      this.loadTasks();
     });
   }
-  closeNewTaskModal(): void {
-  this.newTask = '';
-  this.showNewTaskModal = false;
-}
 
+  closeNewTaskModal(): void {
+    this.newTask = '';
+    this.showNewTaskModal = false;
+    this.cdr.markForCheck();
+  }
 
   deleteTask(id: number): void {
     this.taskService.deleteTask(id).subscribe(() => {
@@ -53,20 +64,25 @@ export class App implements OnInit {
   startEditing(task: Task): void {
     this.editingTaskId = task.id;
     this.updatedTask = task.task;
+    this.cdr.markForCheck();
   }
 
   cancelEditing(): void {
     this.editingTaskId = null;
     this.updatedTask = '';
+    this.cdr.markForCheck();
   }
 
   saveTask(task: Task): void {
     if (this.updatedTask.trim() !== '') {
-      this.taskService.updateTask({ ...task, task: this.updatedTask }).subscribe(() => {
-        task.task = this.updatedTask; 
-        this.editingTaskId = null;
-        this.updatedTask = '';
-      });
+      this.taskService
+        .updateTask({ ...task, task: this.updatedTask })
+        .subscribe(() => {
+          task.task = this.updatedTask;
+          this.editingTaskId = null;
+          this.updatedTask = '';
+          this.cdr.markForCheck();
+        });
     }
   }
 }
